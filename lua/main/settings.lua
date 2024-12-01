9d7fc19547ff091789bd6d384ef76fa03d4c8e4b
denhateu <danilaksenow101@gmail.com>
2024-12-01 20:22:21 +0500

    feat: added tabs settings

--- lua/main/settings.lua
+++ lua/main/settings.lua
@@ -4,6 +4,18 @@ local opt = vim.opt
 -- Также алиас для vim.global
 local g = vim.g
 
+-- [[ Табы ]]
+-- Использует пробелы вместо табов
+opt.expandtab = true
+
+-- Размер таба при нажатии <TAB>
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.softtabstop = 2
+
+-- Подстраивать новые строки под предыдущий отступ
+opt.smartindent = true
+
 -- [[ Поиск ]]
 -- Игнорить регистр при поиске,
 -- т.е. если при поиске написать "JOPA" то он найдет "jopa"
