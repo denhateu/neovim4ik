befefb9a3d3ec667373e29d9dce02c0cb6e73efd
denhateu <danilaksenow101@gmail.com>
2024-12-17 23:02:41 +0500

    chore: added keybinds for nvim-tree

--- lua/keys/keys.lua
+++ lua/keys/keys.lua
@@ -5,3 +5,7 @@ vim.g.mapleader = ","
 vim.g.maplocalleader = "\\"
 
 setkey('i', 'kj', '<ESC>')
+
+-- Горячие клавиши для nvim-tree
+setkey('n', '<leader>b', ':NvimTreeToggle<CR>')
+setkey('n', '<leader>bf', ':NvimTreeFocus<CR>')
