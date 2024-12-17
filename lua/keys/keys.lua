7f617a33796e6d07d5a8166a51747dcd108ecb18
denhateu <danilaksenow101@gmail.com>
2024-12-17 23:39:45 +0500

    chore: added some keymaps for paired characters

--- lua/keys/keys.lua
+++ lua/keys/keys.lua
@@ -6,6 +6,14 @@ vim.g.maplocalleader = "\\"
 
 setkey('i', 'kj', '<ESC>')
 
+-- Мой способ для того чтобы открывать кавычки, скобки и т.д.
+setkey('i', '(', '()<LEFT>')
+setkey('i', '[', '[]<LEFT>')
+setkey('i', '{', '{}<LEFT>')
+setkey('i', '\'', '\'\'<LEFT>')
+setkey('i', '"', '""<LEFT>')
+setkey('i', '<', '<><LEFT>')
+
 -- Горячие клавиши для nvim-tree
 setkey('n', '<leader>b', ':NvimTreeToggle<CR>')
 setkey('n', '<leader>bf', ':NvimTreeFocus<CR>')
