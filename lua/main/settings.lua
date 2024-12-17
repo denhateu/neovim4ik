ebb147d0d1777ed51c857290fc38b863860de6e0
denhateu <danilaksenow101@gmail.com>
2024-12-17 19:34:54 +0500

    chore: added colorcolumn setting and yankhighllight

--- lua/main/settings.lua
+++ lua/main/settings.lua
@@ -64,4 +64,15 @@ opt.backup = false
 opt.showcmd = true
 
 opt.scrolloff = 7
+
+-- Ограничение длины строки кода (выход за линию запрещён!)
+opt.colorcolumn = '119'
+
+-- Подсвечивает на доли секунды скопированную часть текста
+vim.api.nvim_exec([[
+augroup YankHighlight
+autocmd!
+autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=150}
+augroup end
+]], false)
 -- }}
